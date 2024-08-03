8dc703201fae950065d54987f0d4ab63da83fc1a
Richard Bao <richardbao419@gmail.com>
2024-08-03 14:59:41 -0700

    drone API

--- src/robot/drone.py
+++ src/robot/drone.py
@@ -1,8 +1,42 @@
 import numpy as np
-from robot import Robot
-class Drone(Robot):
-    def __init__(self):
-        self.id = None
+from typing import List
+
+class Position:
+     def __init__(self, x, y):
+         self.x = x
+         self.y =y
+
+class Drone():
+    def __init__(self, id: int, x: int, y: int):
+        self._id = None
+        self._position = Position(x,y)
+        self._map = np.zeros(3,3,dtype=int)
+        self._next_moves = []
+        self._dwell_time = 0
+        self._historical_perception_context = ""
+        self._current_perception_context = []
+
+    def get_id(self) -> int:
+        """Get the unique identifier for this drone."""
+        return self._id
+    
+    def get_position(self) -> Position:
+        """Get the position for this drone."""
+        return self._position
+
+    def update(self) -> None:
+        """Plans the next moves"""
+        return
+
+    def can_communicate(self) -> bool:
+        return True
+    
+    def set_neighbors(self) -> None:
+        return
+
+    def add_to_current_perception_context(self, perception_context: List[str]):
+        return
+
 
 if __name__ == "__main__":
     print("hello world")
--- src/robot/robot.py
+++
@@ -1,5 +0,0 @@
-class Robot:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    
